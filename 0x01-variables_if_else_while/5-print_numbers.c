e4c0c23481bcdec1ed6265d83647badda877b4e5
DJ-Man-2099 <davidmesak@hotmail.com>
2023-07-14 13:53:27 -0700

    task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * print whether the number stored in the variable n is positive or negative
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+	printf("%d",a);
+printf("\n");
+return (0);
+}
