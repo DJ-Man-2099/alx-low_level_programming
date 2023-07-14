6f5f247dd19d16bad00fb35b053b0847a3188a34
DJ-Man-2099 <davidmesak@hotmail.com>
2023-07-14 13:50:39 -0700

    task 5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * print whether the number stored in the variable n is positive or negative
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+	if (a != 'e' && a != 'q')
+		putchar(a);
+putchar('\n');
+return (0);
+}
