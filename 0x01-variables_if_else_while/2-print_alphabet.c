bccfe4d24da0207365d4a9bef98583c32c2a263c
DJ-Man-2099 <davidmesak@hotmail.com>
2023-07-14 13:42:37 -0700

    task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * print whether the number stored in the variable n is positive or negative
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a;
+for(a = 'a'; a <= 'z'; a++)
+	putchar(a);
+putchar('\n');
+return (0);
+}
