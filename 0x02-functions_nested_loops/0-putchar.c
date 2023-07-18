65e560840455fe5e707ae29694b617579bbbfee3
DJ-Man-2099 <davidmesak@hotmail.com>
2023-07-17 21:40:34 -0700

    task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * it prints exactly
+ * '_putchar'
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("_putchar");
+return (0);
+}
