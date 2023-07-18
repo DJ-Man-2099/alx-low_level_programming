31300a56adc42b8b6b8f041f88df9f229352f9bd
DJ-Man-2099 <davidmesak@hotmail.com>
2023-07-18 12:42:15 -0700

    task 8

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 #include <stdio.h>
 /**
  * main - check the code
@@ -9,9 +10,7 @@ int main(void)
 {
 int r;
 
-print_last_digit(98);
-print_last_digit(0);
-r = print_last_digit(-1024);
+r = print_last_digit(r);
 _putchar('0' + r);
 _putchar('\n');
 
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,9 +11,9 @@
 int print_last_digit(int n)
 {
 int result;
-if (n < 0)
-	n = -n;
 result = n % 10;
+if (result < 0)
+	result = -result;
 _putchar('0' + result);
 return (result);
 }
