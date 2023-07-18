fe590a5abccb6f0859ac4efe9e930147ab0a7051
DJ-Man-2099 <davidmesak@hotmail.com>
2023-07-17 21:57:11 -0700

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -10,6 +11,9 @@
  */
 int main(void)
 {
-puts("_putchar");
+char* word = "_putchar\n";
+int i;
+for(i = 0; i < 9; i++)
+	_putchar(word[i]);
 return (0);
 }
