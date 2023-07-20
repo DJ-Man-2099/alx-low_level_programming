b24247bdd24e0f77ae0a3415b4750de62a8908de
DJ-Man-2099 <davidmesak@hotmail.com>
2023-07-20 05:13:38 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - math function.
+ * @a: first number
+ * @b: second number
+ *
+ * multiplies a and b
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
