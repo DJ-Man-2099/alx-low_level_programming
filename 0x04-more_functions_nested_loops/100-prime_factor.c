75972ab702e9bea3ab92d99487683dd015a0100a
DJ-Man-2099 <davidmesak@hotmail.com>
2023-07-20 16:19:25 -0700

    optional task 1

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - interview function.
+ *
+ * prints the numbers from 1 to 100,
+ * followed by a new line.
+ * But for multiples of three print Fizz instead of the number
+ * for the multiples of five print Buzz.
+ * For numbers which are multiples of both three and five print FizzBuzz.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int i, value, limit;
+
+	value = 612852475143;
+	limit = sqrt(value);
+
+	for (i = 3; i <= limit; i += 2)
+	{
+		while (value % i == 0)
+		{
+			value /= i;
+		}
+	}
+	printf("%ld\n", i);
+	return (0);
+}
