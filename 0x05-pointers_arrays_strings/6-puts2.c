a726e7fef3f3c6ccda7a6495706bca4f3dcb0eab
DJ-Man-2099 <davidmesak@hotmail.com>
2023-07-24 12:15:41 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdbool.h>
+/**
+ * temp_print - temp function.
+ * @str: pointer to a string
+ * @print: condition of print
+ *
+ * prints a string, even chars
+ * through recursion
+ *
+ * Return: void
+ */
+void temp_print(char *str, bool print)
+{
+	char *next;
+
+	if (*str != '\0')
+	{
+		next = str + 1;
+		if (print)
+			_putchar(*str);
+		temp_print(next, !print);
+	}
+}
+/**
+ * print_rev - pointer function.
+ * @s: pointer to a string
+ *
+ * prints a string, in reverse
+ * followed by a new line
+ *
+ * Return: void
+ */
+void puts2(char *s)
+{
+	temp_print(s, true);
+	_putchar('\n');
+}
