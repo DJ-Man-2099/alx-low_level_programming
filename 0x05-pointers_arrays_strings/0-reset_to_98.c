43e03134b6a47b214a31ddda0c1706672c1fee0d
DJ-Man-2099 <davidmesak@hotmail.com>
2023-07-24 10:24:42 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - printer function.
+ * @n: pointer to an integer
+ *
+ * updates the value n points to to 98
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
