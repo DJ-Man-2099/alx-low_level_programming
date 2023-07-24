94b42b6ddf9f056bed44401a600906a4a424e9d7
DJ-Man-2099 <davidmesak@hotmail.com>
2023-07-24 13:04:46 -0700

    task 10

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,12 +15,12 @@ char *_strcpy(char *dest, char *src)
 	char c, *base;
 
 	base = dest;
-	while (*src != '\0')
-	{
+	do {
 		c = *src;
 		*base = c;
 		src++;
 		base++;
-	}
+	} while (*src != '\0');
+
 	return (dest);
 }
