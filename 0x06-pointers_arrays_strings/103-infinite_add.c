ebb1d1877e67e8f35d101b412c4b80949d59557b
DJ-Man-2099 <davidmesak@hotmail.com>
2023-07-28 07:45:16 -0700

    optional task 4

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,44 @@
 #include "main.h"
 #include <stdbool.h>
 #include <stdio.h>
+/**
+ * temp_add - string function
+ * @n1: pointer to first integer
+ * @n2:	pointer to second integer
+ * @baseN1: start of n1
+ * @baseN2: start of n2
+ * @r: pointer to buffer
+ * @baseR: start of r
+ * @carry: remainder
+ *
+ * adds two numbers, stores result in r
+ *
+ * Return: if result is valid
+ */
+bool temp_add(char *n1, char *n2, char *baseN1,
+char *baseN2, char **r, char *baseR, int *carry)
+{
+	int i1, i2;
 
+	while (n1 >= baseN1)
+	{
+		if (*r <= baseR + 1)
+			return (false);
+		i1 = *n1 - '0';
+		if (n2 < baseN2)
+			i2 = 0;
+		else
+			i2 = *n2 - '0';
+		**r = '0' + (((i1 + i2 + *carry) % 10));
+		*carry = (i1 + i2 + *carry) / 10;
+		n1--;
+		if (n2 >= baseN2)
+			n2--;
+		(*r)--;
+	}
+	printf("%s\n", *r);
+	return (true);
+}
 /**
  * infinite_add - string function
  * @n1: pointer to first integer
@@ -16,9 +53,9 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int carry = 0, i1, i2;
-	bool n1Bigger;
 	char *baseN1 = n1, *baseN2 = n2, *baseR = r;
+	int carry = 0;
+	bool n1Bigger, valid;
 
 	while (*(n1 + 1) != '\0')
 		n1++;
@@ -30,45 +67,22 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	r--;
 	if (n1Bigger)
 	{
-		while (n1 >= baseN1)
-		{
-			if (r <= baseR + 1)
-				return (0);
-			i1 = *n1 - '0';
-			if (n2 < baseN2)
-				i2 = 0;
-			else
-				i2 = *n2 - '0';
-			*r = '0' + (((i1 + i2 + carry) % 10));
-			carry = (i1 + i2 + carry) / 10;
-			n1--;
-			if (n2 >= baseN2)
-				n2--;
-			r--;
-		}
+		printf("N1 > n2\n");
+		valid = temp_add(n1, n2, baseN1, baseN2, &r, baseR, &carry);
+		if (!valid)
+			return (0);
 	}
 	else
 	{
-		while (n2 >= baseN2)
-		{
-			if (r <= baseR + 1)
-				return (0);
-			i2 = *n2 - '0';
-			if (n1 < baseN1)
-				i1 = 0;
-			else
-				i1 = *n1 - '0';
-			*r = '0' + (((i1 + i2 + carry) % 10));
-			carry = (i1 + i2 + carry) / 10;
-			n2--;
-			if (n1 >= baseN1)
-				n1--;
-			r--;
-		}
+		printf("N2 > n1\n");
+		valid = temp_add(n2, n1, baseN2, baseN1, &r, baseR, &carry);
+		if (!valid)
+			return (0);
 	}
 	if (carry == 1)
 		*r = '0' + carry;
 	else
 		r++;
+	printf("%s\n", r);
 	return (r);
 }
