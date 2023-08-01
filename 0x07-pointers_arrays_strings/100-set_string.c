97acdf56f679ce8aed7b81c190247395026a10da
DJ-Man-2099 <davidmesak@hotmail.com>
2023-08-01 11:10:42 -0700

    optional task 1

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdbool.h>
+#include <stdio.h>
+/**
+ * set_string - string function
+ * @s: pointer to string to replace
+ * @to: pointer to string to replace with
+ *
+ * sets the value of a pointer to a char
+ *
+ * return: null
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
