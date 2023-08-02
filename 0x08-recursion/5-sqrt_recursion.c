4e70d98c931c3fdb15b251d97092875abeefbd9b
DJ-Man-2099 <davidmesak@hotmail.com>
2023-08-02 23:02:42 +0300

    task 6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * _temp_recursion - pointer function.
+ * @n: the integer to get its sqrt
+ * @test: the integer to test with
+ *
+ * returns the square root of n
+ *
+ * Return: sqrt(n)
+ */
+int _temp_recursion(int n, int test)
+{
+	if (test * test == n)
+	{
+		return (test);
+	}
+	if (test > n)
+	{
+		return (-1);
+	}
+
+	return (_temp_recursion(n, test + 1));
+}
+/**
  * _sqrt_recursion - pointer function.
  * @n: the integer to get its sqrt
  *
@@ -10,20 +32,12 @@
  */
 int _sqrt_recursion(int n)
 {
-	int i;
-
 	if (n < 0)
 		return (-1);
 	if (n == 0)
 		return (0);
 	if (n == 1)
 		return (1);
-	for (i = 0; i < n; i++)
-	{
-		if (i * i == n)
-		{
-			return (i);
-		}
-	}
-	return (-1);
+
+	return (_temp_recursion(n, 2));
 }
