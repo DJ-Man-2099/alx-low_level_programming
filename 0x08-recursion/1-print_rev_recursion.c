cada8665f797467615a8447695bfc5dea6e117c1
DJ-Man-2099 <davidmesak@hotmail.com>
2023-08-02 10:12:53 +0300

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * temp_print - temp function.
+ * @str: pointer to a string
+ *
+ * prints a string in reverse
+ * through recursion
+ *
+ * Return: void
+ */
+void temp_print(char *str)
+{
+	char *next;
+
+	if (*str != '\0')
+	{
+		next = str + 1;
+		temp_print(next);
+		_putchar(*str);
+	}
+}
+/**
+ * _puts_recursion - pointer function.
+ * @s: pointer to a string
+ *
+ * prints a string, in reverse
+ * followed by a new line
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	temp_print(s);
+	_putchar('\n');
+}
