85d454ea3d0113685445a4fb9684dffe93c7433e
DJ-Man-2099 <davidmesak@hotmail.com>
2023-08-05 02:11:48 +0300

    task 1

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,6 +1,25 @@
 #include "main.h"
 #include <stdbool.h>
 #include <stdio.h>
+/**
+ * convert - temp function.
+ * @str: pointer to string
+ * @number: pointer to number
+ * @decimal: pointer to decimal
+ *
+ * converts string to integer
+ *
+ * Return: null
+ */
+void convert(char *str, unsigned int *number, unsigned int *decimal)
+{
+	if (*str >= '0' && *str <= '9')
+	{
+		convert(str + 1, number, decimal);
+		*number += (*str - '0') * (*decimal);
+		(*decimal) *= 10;
+	}
+}
 
 /**
  * _atoi - pointer function.
@@ -27,6 +46,7 @@ int _atoi(char *s)
 		}
 		else if (*s >= '0' && *s <= '9')
 		{
+			convert(s, &number, &decimal);
 			break;
 		}
 		s++;
