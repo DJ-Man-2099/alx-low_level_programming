c5a1588810102c9092c7815e276a544df8093a55
DJ Man <49337662+DJ-Man-2099@users.noreply.github.com>
2023-08-10 15:06:36 +0300

    task 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ int _strlen(char *s)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr, *base_s1 = s1, *base_s2 = s2, *base_ptr;
-	unsigned int i, s1_length, s2_length;
+	unsigned int i, s1_length;
 
 	if (s1 == NULL)
 	{
@@ -46,7 +46,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		base_s2 = "";
 	}
 	s1_length = _strlen(base_s1);
-	s2_length = _strlen(base_s2);
 	ptr = malloc((n + s1_length) * (sizeof(*ptr)));
 	if (ptr == NULL)
 	{
