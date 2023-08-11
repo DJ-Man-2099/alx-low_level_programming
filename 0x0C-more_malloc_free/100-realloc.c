ef97d29431ce9d0fea23638ebc6d311b76a7d145
DJ-Man-2099 <davidmesak@hotmail.com>
2023-08-12 00:51:31 +0300

    optional task 1

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,7 +2,7 @@
 #include "main.h"
 #include <string.h>
 /**
- * _realloc - meory function
+ * _realloc - memory function
  * @ptr: pointer to old memory
  * @old_size: size of old memory
  * @new_size: size of new memory
@@ -39,7 +39,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (new_ptr);
 	}
 
-	memcpy(new_ptr, ptr, old_size);
+	memcpy(new_ptr, ptr, size);
 
 	free(ptr);
 
