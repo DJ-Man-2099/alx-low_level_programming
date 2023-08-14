ab209f18d7af1bfc689ce60f571c2c5cc99a51ff
DJ-Man-2099 <davidmesak@hotmail.com>
2023-08-14 23:17:56 +0300

    task 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Structure for a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: This structure holds the information about a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
