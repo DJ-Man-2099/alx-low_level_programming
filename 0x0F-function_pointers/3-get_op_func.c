b22fb1974309e96c00fa6efa89bea4d3f577613a
DJ-Man-2099 <davidmesak@hotmail.com>
2023-08-17 01:03:21 +0300

    task 4

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,5 @@
 #include "3-calc.h"
 #include <stdlib.h>
-#include <string.h>
 /**
  * get_op_func - point to func
  * @s: op to compare
@@ -22,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (*ops[i].op == *s)
 		{
 			return (ops[i].f);
 		}
