ac0f15465d0a53d4eda2e52860c729134825bc65
DJ-Man-2099 <davidmesak@hotmail.com>
2023-08-16 23:51:26 +0300

    task 1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name - func pointer function
+ * @name: string to print
+ * @f: function to use to print
+ *
+ * calls f(name)
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
