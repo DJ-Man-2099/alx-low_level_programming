48f955210457ae1f207c8a7108b1ded095815071
DJ Man <49337662+DJ-Man-2099@users.noreply.github.com>
2023-08-17 13:35:08 +0300

    task 4

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -30,48 +30,51 @@ void *get_value(char const format, void *string)
  */
 void print_all(const char *const format, ...)
 {
-	unsigned int i = 0;
-	va_list string_array;
-	char temp_format[] = "%c";
-
-	va_start(string_array, format);
-
-	while (!(format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
+	if (format != NULL)
 	{
-		printf("%c", format[i]);
-		i++;
-	}
-	sprintf(temp_format, "%%%c", format[i]);
-	switch (format[i])
-	{
-	case 'f':
-		printf(temp_format, va_arg(string_array, double));
-		break;
+		unsigned int i = 0;
+		va_list string_array;
+		char temp_format[] = "%c";
 
-	default:
-		printf(temp_format, get_value(format[i], va_arg(string_array, void *)));
-		break;
-	}
-	i++;
-	while (format[i] != '\0')
-	{
-		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+		va_start(string_array, format);
+
+		while (!(format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
 		{
-			printf(", ");
-			sprintf(temp_format, "%%%c", format[i]);
-			switch (format[i])
-			{
-			case 'f':
-				printf(temp_format, va_arg(string_array, double));
-				break;
+			printf("%c", format[i]);
+			i++;
+		}
+		sprintf(temp_format, "%%%c", format[i]);
+		switch (format[i])
+		{
+		case 'f':
+			printf(temp_format, va_arg(string_array, double));
+			break;
 
-			default:
-				printf(temp_format, get_value(format[i], va_arg(string_array, void *)));
-				break;
-			}
+		default:
+			printf(temp_format, get_value(format[i], va_arg(string_array, void *)));
+			break;
 		}
 		i++;
+		while (format[i] != '\0')
+		{
+			if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+			{
+				printf(", ");
+				sprintf(temp_format, "%%%c", format[i]);
+				switch (format[i])
+				{
+				case 'f':
+					printf(temp_format, va_arg(string_array, double));
+					break;
+
+				default:
+					printf(temp_format, get_value(format[i], va_arg(string_array, void *)));
+					break;
+				}
+			}
+			i++;
+		}
+		va_end(string_array);
+		printf("\n");
 	}
-	va_end(string_array);
-	printf("\n");
 }
