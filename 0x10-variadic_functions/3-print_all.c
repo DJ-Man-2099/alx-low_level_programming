abc648ba25f06472ed7550644c2cffde645299f5
DJ-Man-2099 <davidmesak@hotmail.com>
2023-08-17 23:13:24 +0300

    task 4

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,7 +2,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+/**
+ * print_value - variadic function
+ * @string_array: string
+ * @format: format of string
+ *
+ * gets the required argument
+ *
+ * Return: void *
+ */
+void print_value(va_list *string_array, char format)
+{
+	char *temp;
 
+	switch (format)
+	{
+	case 'i':
+		printf("%d", va_arg(*string_array, int));
+		break;
+	case 'f':
+		printf("%f", va_arg(*string_array, double));
+		break;
+	case 's':
+		temp = va_arg(*string_array, char *);
+		if (temp == NULL)
+		{
+			temp = "(nil)";
+		}
+		printf("%s", temp);
+		break;
+	default:
+		printf("%c", (char)va_arg(*string_array, int));
+		break;
+	}
+}
 /**
  * print_all - variadic function
  * @format: list of types of arguments passed to the function
@@ -15,38 +48,36 @@
 void print_all(const char *const format, ...)
 {
 	unsigned int i = 0;
-	va_list lists;
-	char *string;
+	va_list string_array;
 
-	va_start(lists, format);
-	while (format && format[i])
+	va_start(string_array, format);
+	if (format != NULL)
 	{
-		switch (format[i])
+		while (!(format[i] == 'c' || format[i] == 'i'
+		|| format[i] == 'f' || format[i] == 's'))
 		{
-		case 'c':
-			printf("%c", (char)va_arg(lists, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(lists, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(lists, double));
-			break;
-		case 's':
-			string = va_arg(lists, char *);
-			if (string == NULL)
-			{
-				string = "(nil)";
-			}
-			printf("%s", string);
-			break;
+			i++;
 		}
-		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
-			 format[i] == 's') &&
-			format[(i + 1)] != '\0')
-			printf(", ");
+		print_value(&string_array, format[i]);
 		i++;
+		while (format[i] != '\0')
+		{
+			switch (format[i])
+			{
+			case 'c':
+			case 'i':
+			case 'f':
+			case 's':
+				printf(", ");
+				print_value(&string_array, format[i]);
+				break;
+
+			default:
+				break;
+			}
+			i++;
+		}
 	}
+	va_end(string_array);
 	printf("\n");
-	va_end(lists);
 }
