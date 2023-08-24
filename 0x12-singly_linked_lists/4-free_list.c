8c6586569f85fbb76e16b48abcb4b01e26c14d16
DJ-Man-2099 <davidmesak@hotmail.com>
2023-08-24 23:11:04 +0300

    task 5

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - data structure function
+ * @head: node to free
+ *
+ * frees list
+ *
+ * Return: number of nodes
+ */
+void free_list(list_t *head)
+{
+	if (head->next != NULL)
+	{
+		free_list(head->next);
+	}
+	free(head);
+}
