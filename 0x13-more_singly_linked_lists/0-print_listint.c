ee4ea6e10abcbada857e9c2ab74d8d6b7f49b571
DJ Man <49337662+DJ-Man-2099@users.noreply.github.com>
2023-08-28 09:24:36 +0300

    task 1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * print_listint - data structure function
+ * @h: current node
+ *
+ * prints the list of nodes
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t len = 0;
+	const listint_t *current = h;
+
+	while (current != NULL)
+	{
+		if (current->n != NULL)
+		{
+			printf("%d", current->n);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		len++;
+		current = current->next;
+	}
+	return (len);
+}
