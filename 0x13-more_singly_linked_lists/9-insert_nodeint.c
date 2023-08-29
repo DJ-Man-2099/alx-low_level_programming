b396fa567f6f7acde3b69f92f3debb7ca66e5a13
DJ Man <49337662+DJ-Man-2099@users.noreply.github.com>
2023-08-29 09:22:37 +0300

    task 10

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_head;
 		return (new_head);
 	}
-	if ((head != NULL) && *head != NULL)
+	if (head != NULL && *head != NULL)
 	{
 		current = *head;
 		while (current != NULL)
