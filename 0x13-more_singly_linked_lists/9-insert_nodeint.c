765b106a52d45bcb050043d59cbfec1ce77cbbf5
DJ Man <49337662+DJ-Man-2099@users.noreply.github.com>
2023-08-29 09:02:44 +0300

    task 10

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				*head = new_head;
 				return (new_head);
 			}
-			else if (len == idx - 1)
+			else if (len == idx - 1 && current->next != NULL)
 			{
 				new_head = malloc(sizeof(listint_t));
 				if (new_head == NULL)
