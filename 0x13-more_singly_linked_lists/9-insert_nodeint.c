71faf42e23da7e6c29e9122a3dc752e6d76f560f
DJ Man <49337662+DJ-Man-2099@users.noreply.github.com>
2023-08-29 09:27:50 +0300

    task 10

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,52 +1,42 @@
 #include "lists.h"
+#include <stdlib.h>
+
 /**
- * insert_nodeint_at_index - data structure function
- * @head: current node
- * @idx: index of inserted node
- * @n: value of inserted node
+ * insert_nodeint_at_index - inserting a new node at agiven postion
+ * @head: double pointer
+ * @idx: index of the node
+ * @n: new node value
  *
- * returns the nth node of a listint_t linked list
- *
- * Return: node at n
+ * Return: the address of new node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	size_t len = 0;
-	listint_t *current, *new_head;
+	unsigned int i;
+	listint_t *temp, *new;
 
-	if (head != NULL && *head != NULL)
+	if (head == NULL)
+		return (NULL);
+	if (idx != 0)
 	{
-		current = *head;
-		if (idx == 0)
+		temp = *head;
+		for (i = 0; i < idx - 1 && temp != NULL; i++)
 		{
-			new_head = malloc(sizeof(listint_t));
-			if (new_head == NULL)
-			{
-				return (NULL);
-			}
-			new_head->n = n;
-			new_head->next = current;
-			*head = new_head;
-			return (new_head);
+			temp = temp->next;
 		}
-		while (current != NULL)
-		{
-
-			if (len == idx - 1)
-			{
-				new_head = malloc(sizeof(listint_t));
-				if (new_head == NULL)
-				{
-					return (NULL);
-				}
-				new_head->n = n;
-				new_head->next = current->next;
-				current->next = new_head;
-				return (new_head);
-			}
-			len++;
-			current = current->next;
-		}
+		if (temp == NULL)
+			return (NULL);
 	}
-	return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	new->next = temp->next;
+	temp->next = new;
+	return (new);
 }
