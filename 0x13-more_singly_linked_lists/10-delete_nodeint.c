7164f386b3e87d6337d060a7b46eba78c4a77704
DJ-Man-2099 <davidmesak@hotmail.com>
2023-08-30 18:20:02 +0300

    task 11

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,16 +15,16 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (head != NULL)
 	{
-		if (index == 0)
-		{
-			new_head = (*head)->next;
-			free(*head);
-			*head = new_head;
-			return (1);
-		}
 		while (current != NULL)
 		{
-			if (len == index - 1 && index != 0 && current->next != NULL)
+			if (index == 0)
+			{
+				new_head = (*head)->next;
+				free(*head);
+				*head = new_head;
+				return (1);
+			}
+			else if (len == index - 1 && index != 0 && current->next != NULL)
 			{
 				new_head = current->next->next;
 				free(current->next);
