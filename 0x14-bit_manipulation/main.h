e8d2ca7eb3b2099a5b9c9a269933193ae733f2fd
DJ-Man-2099 <davidmesak@hotmail.com>
2023-08-31 18:30:11 +0300

    task 1

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int _putchar(char c);
+void print_binary(unsigned long int n);
+unsigned int binary_to_uint(const char *b);
+#endif
