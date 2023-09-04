73f413f9a46c5f5fa2b8e2b70943ae4daf1844eb
DJ-Man-2099 <davidmesak@hotmail.com>
2023-09-04 23:26:05 +0300

    task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,14 +12,13 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file;
-	char *end_of_file = {'\0'};
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	file = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (file == -1)
 	{
@@ -29,7 +28,6 @@ int create_file(const char *filename, char *text_content)
 	{
 		write(file, text_content, strlen(text_content));
 	}
-	write(file, end_of_file, 1);
 	close(file);
 	return (1);
 }
