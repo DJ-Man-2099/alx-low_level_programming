8ac9041290efef0e49910e35f08f269c01a0633e
DJ-Man-2099 <davidmesak@hotmail.com>
2023-09-05 02:14:47 +0300

    task 4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -43,9 +43,15 @@ int cp_between_files(const char *file_from,
 	}
 	file_to_fd = open(file_to, O_WRONLY | O_CREAT | O_TRUNC,
 					  S_IREAD | S_IWUSR | S_IWGRP);
+	if (file_to_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		close_file(file_from_fd);
+		return (99);
+	}
 	while ((bytes_read = read(file_from_fd, buf, 1024)) > 0)
 	{
-		if (file_to_fd == -1 || write(file_to_fd, buf, bytes_read) != bytes_read)
+		if (write(file_to_fd, buf, bytes_read) != bytes_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			close_file(file_from_fd);
