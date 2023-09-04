8a6b1ce679015f8bc895bddf2960ae68e503595f
DJ-Man-2099 <davidmesak@hotmail.com>
2023-09-04 23:14:30 +0300

    task 2

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * create_file - file io function
+ * @filename: name of file to read
+ * @text_content: chars to write
+ *
+ * writes text to a file
+ *
+ * Return: 1 on Success,
+ * -1 on error
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	char *end_of_file = {'\0'};
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	file = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+
+	if (file == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		write(file, text_content, strlen(text_content));
+	}
+	write(file, end_of_file, 1);
+	close(file);
+	return (1);
+}
