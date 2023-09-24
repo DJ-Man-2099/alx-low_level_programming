f2cbed4ec694ec49ee3a5fb79c2d9cf78e33ee1d
DJ-Man-2099 <davidmesak@hotmail.com>
2023-09-24 12:29:28 +0300

    optional task 5

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-#include <math.h>
 /**
  * main - prints sum of natural numbers
  * that are below 1024
@@ -10,22 +9,22 @@
  */
 int main(void)
 {
-int i;
-double a, b, sum;
-a = 1;
-b = 2;
-printf("%.0f, %.0f, ", a, b);
-for (i = 2; i < 98; i++)
-{
-	sum = ceil(a) + ceil(b);
-	printf("%.0f", ceil(sum));
-	if (i != 97)
+	int i;
+	double a, b, sum;
+	a = 1;
+	b = 2;
+	printf("%.0f, %.0f, ", a, b);
+	for (i = 2; i < 98; i++)
 	{
-		printf(", ");
+		sum = a + b;
+		printf("%.0f", sum);
+		if (i != 97)
+		{
+			printf(", ");
+		}
+		a = b;
+		b = sum;
 	}
-	a = ceil(b);
-	b = ceil(sum);
-}
-printf("\n");
-return (0);
+	printf("\n");
+	return (0);
 }
