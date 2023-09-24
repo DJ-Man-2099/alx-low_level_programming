4f5c8db35972793fe13d227afea167079f581d52
DJ-Man-2099 <davidmesak@hotmail.com>
2023-09-24 16:16:51 +0300

    optional task 5

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#define LIMIT 1000000000
 /**
  * main - prints sum of natural numbers
  * that are below 1024
@@ -10,21 +11,37 @@
 int main(void)
 {
 	int i;
-	double a, b, sum;
+	long int low_a, low_b, low_sum = 0;
+	long int high_a = 0, high_b = 0, high_sum = 0;
 
-	a = 1;
-	b = 2;
-	printf("%.0f, %.0f, ", a, b);
+	low_a = 1;
+	low_b = 2;
+	printf("%ld, %ld, ", low_a, low_b);
 	for (i = 2; i < 98; i++)
 	{
-		sum = a + b;
-		printf("%.0f", sum);
+		if (low_a > LIMIT)
+		{
+			high_a += low_a / LIMIT;
+			low_a = low_a % LIMIT;
+		}
+		if (low_b > LIMIT)
+		{
+			high_b += low_b / LIMIT;
+			low_b = low_b % LIMIT;
+		}
+		high_sum = high_a + high_b;
+		if (high_sum > 0)
+			printf("%ld", high_sum);
+		low_sum = low_a + low_b;
+		printf("%ld", low_sum);
 		if (i != 97)
 		{
 			printf(", ");
 		}
-		a = b;
-		b = sum;
+		low_a = low_b;
+		low_b = low_sum;
+		high_a = high_b;
+		high_b = high_sum;
 	}
 	printf("\n");
 	return (0);
