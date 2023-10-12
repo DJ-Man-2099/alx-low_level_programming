8c683a7092be9debc4919578d6db48e7ff3d326f
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-12 19:46:33 +0300

    Task 4

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -21,7 +21,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		while (current->next != NULL)
 			current = current->next;
 		current->next = new_node;
-		new_node->prev = *head;
+		new_node->prev = current;
 	}
 	else
 		(*head) = new_node;
