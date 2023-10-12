21a42a8aa7504c9d12bdff9018561685efebd782
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-12 20:59:47 +0300

    Task 8

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current = *h, *new_node;
+	dlistint_t *current = *h, *new_node, *prev = *h;
 	size_t current_index;
 
 	for (current_index = 0;
@@ -35,7 +35,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				*h = new_node;
 			return (new_node);
 		}
+		prev = current;
 		current = current->next;
+	}
+	if (current_index == idx)
+	{
+		new_node = malloc(sizeof(dlistint_t));
+		if (new_node == NULL)
+			return (NULL);
+
+		new_node->n = n;
+		new_node->prev = prev;
+		prev->next = new_node;
+		return (new_node);
 	}
 
 	return (NULL);
