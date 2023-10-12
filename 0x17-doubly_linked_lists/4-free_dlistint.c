594b46f08ae68d06e33af62851637f8a753e5cc6
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-12 19:53:28 +0300

    Task 5

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - dlinklist function
+ * @head: start of node
+ *
+ * frees list
+ *
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head, *next_current = head;
+
+	while (current != NULL)
+	{
+		next_current = current->next;
+		free(current);
+		current = next_current;
+	}
+}
