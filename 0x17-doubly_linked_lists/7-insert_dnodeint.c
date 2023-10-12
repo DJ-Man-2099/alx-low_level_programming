1b66386063da2f6d81473bc0d1decc164714df26
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-12 20:58:30 +0300

    Task 8

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current = *h, *new_node, *prev = *h;
+	dlistint_t *current = *h, *new_node;
 	size_t current_index;
 
 	for (current_index = 0;
