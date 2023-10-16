9c0dd3181f9f2766bfec597251f8b77eff505dc2
DJ-Man-2099 <davidmesak@hotmail.com>
2023-10-16 21:11:58 +0300

    Optional Task 2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. "https://github.com/DJ-Man-2099/alx-low_level_programming/blob/main/0x18-dynamic_libraries/101-fake_rand.so"
+wget -P .. "https://raw.githubusercontent.com/DJ-Man-2099/alx-low_level_programming/main/0x18-dynamic_libraries/101-fake_rand.so"
 export LD_PRELOAD="$PWD/../101-fake_rand.so"
