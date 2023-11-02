3f21764c9972fc77cefda3007d9199c711cc1ee7
DJ-Man-2099 <davidmesak@hotmail.com>
2023-11-03 01:56:02 +0200

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -40,6 +40,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (ht->array[index]->value == NULL)
 			return (0);
 		ht->array[index]->value = strcpy(ht->array[index]->value, value);
+		ht->array[index]->next = NULL;
 	}
 	return (1);
 }
