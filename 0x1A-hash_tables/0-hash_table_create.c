1216b7f1c573ae64d941b458794c08defad943b1
DJ-Man-2099 <davidmesak@hotmail.com>
2023-11-03 01:43:22 +0200

    Task 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,11 +8,17 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = calloc(1, sizeof(hash_table_t));
-	hash_node_t **array = calloc(size, sizeof(hash_node_t));
+	hash_table_t *table;
+	hash_node_t **array;
 
-	if (table == NULL || array == NULL)
+	if (table = calloc(1, sizeof(hash_table_t)) == NULL)
 		return (NULL);
+
+	if (array = calloc(size, sizeof(hash_node_t)) == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 
 	table->size = size;
 	table->array = array;
