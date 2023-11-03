e91ee20fd73293176f83109c2ae866e638bcb43a
DJ-Man-2099 <davidmesak@hotmail.com>
2023-11-03 04:11:32 +0200

    Task 4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,44 +13,32 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index, size = ht->size;
+	unsigned long int index;
 	hash_node_t *new_node, *current;
 
 	if (strlen(key) == 0 || ht == NULL ||
 		key == NULL || value == NULL)
 		return (FAIL);
-	index = (hash_djb2((const unsigned char *)key) % (ht->size));
-	if (ht->array[index] == NULL) /*slot is empty, put node here*/
-	{
-		new_node = malloc(sizeof(hash_node_t));
-		if (new_node == NULL)
-			return (0);
-		ht->array[index] = new_node;
-		new_node->key = (strdup(key));
-		new_node->value = (strdup(value));
-		new_node->next = NULL;
-		return (1);
-	}
+	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
 
-	while (current) /*while "head" is not NULL*/
+	while (current != NULL)
 	{
-		if (strcmp(key, current->key) == 0)
+		if (strcmp(current->key, key) == 0)
 		{
 			free(current->value);
 			current->value = (strdup(value));
-			return (1);
+			return (SUCCESS);
 		}
-		current = current->next; /*traversing*/
+		current = current->next;
 	}
-	current = ht->array[index];
-
-	new_node = malloc(sizeof(hash_node_t));
+	new_node = calloc(1, sizeof(hash_node_t));
 	if (new_node == NULL)
-		return (0);
+		return (FAIL);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-	new_node->key = (strdup(key));
-	new_node->value = (strdup(value));
-	new_node->next = current, ht->array[index] = new_node;
-	return (1);
+
+	return (SUCCESS);
 }
