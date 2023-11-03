68d5a563b395d69155e35fec3f93bb5fa290e450
DJ-Man-2099 <davidmesak@hotmail.com>
2023-11-03 04:06:19 +0200

    Task 4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index, size = ht->size;
-	hash_node_t *new_node = NULL, *current;
+	hash_node_t *new_node, *current;
 
 	if (strlen(key) == 0 || ht == NULL ||
 		key == NULL || value == NULL)
@@ -37,10 +37,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (FAIL);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	if (ht->array[index] == NULL)
-		new_node->next = NULL;
-	else
-		new_node->next = ht->array[index];
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
 	return (SUCCESS);
