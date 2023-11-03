28c837807d6a94d05915b0cb63b6992ea8e76579
DJ-Man-2099 <davidmesak@hotmail.com>
2023-11-03 04:08:56 +0200

    Yasl

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -43,13 +43,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		current = current->next; /*traversing*/
 	}
-	new_node = calloc(1, sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (FAIL);
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+	current = ht->array[index];
 
-	return (SUCCESS);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	ht->array[index] = new_node;
+	new_node->key = (strdup(key));
+	new_node->value = (strdup(value));
+	new_node->next = current, ht->array[index] = new_node;
+	return (1);
 }
