b958ca4623bcd8548ed5dc7fad4307d8a7311a86
DJ-Man-2099 <davidmesak@hotmail.com>
2023-11-03 02:37:08 +0200

    Task 4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,7 @@
 #include "hash_tables.h"
 #include <stdio.h>
+#define FAIL 0
+#define SUCCESS 1
 /**
  * hash_table_set - hash function
  * @ht: hash table
@@ -15,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL, *current;
 
 	if (strlen(key) == 0 || ht == NULL || ht->size == 0)
-		return (0);
+		return (FAIL);
 	index = key_index((const unsigned char *)key, size);
 	current = ht->array[index];
 
@@ -36,15 +38,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		new_node = calloc(1, sizeof(hash_node_t));
 		if (new_node == NULL)
-			return (0);
+			return (FAIL);
 		new_node->key = (char *)key;
 		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
 	}
 	new_node->value = calloc(1 + strlen(value), sizeof(char));
 	if (new_node->value == NULL)
-		return (0);
+		return (FAIL);
 	new_node->value = strcpy(new_node->value, value);
 
-	return (1);
+	return (SUCCESS);
 }
