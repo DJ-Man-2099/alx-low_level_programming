8ca5be36244ab8538b8b6a16ad72aef0caedefb2
DJ-Man-2099 <davidmesak@hotmail.com>
2023-11-03 04:03:12 +0200

    Task 4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,11 +32,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		current = current->next;
 	}
-	new_node = malloc(sizeof(hash_node_t));
+	new_node = calloc(1, sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (FAIL);
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
+	new_node->key = (strdup(key));
+	new_node->value = (strdup(value));
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
