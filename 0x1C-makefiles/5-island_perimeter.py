cb4e68c7c9a38ef88e8230f2a3382951958b0430
DJ-Man-2099 <davidmesak@hotmail.com>
2023-12-20 22:35:23 +0200

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,13 +7,17 @@ def island_perimeter(grid):
     for row_index in range(len(grid)):
         for column_index in range(len(grid[row_index])):
             if grid[row_index][column_index] == 1:
-                if row_index == 0 or (row_index != 0 and grid[row_index-1][column_index] == 0):
+                if row_index == 0 \
+                        or (row_index != 0
+                            and grid[row_index-1][column_index] == 0):
                     perimeter += 1
                 if row_index == len(grid)-1 \
-                        or (row_index < len(grid)-1 and grid[row_index+1][column_index] == 0):
+                        or (row_index < len(grid)-1
+                            and grid[row_index+1][column_index] == 0):
                     perimeter += 1
                 if column_index == 0 \
-                        or column_index != 0 and grid[row_index][column_index-1] == 0:
+                        or column_index != 0 and \
+                        grid[row_index][column_index-1] == 0:
                     perimeter += 1
                 if row_index == len(grid[row_index])-1 \
                         or column_index < len(grid[row_index])-1 \
