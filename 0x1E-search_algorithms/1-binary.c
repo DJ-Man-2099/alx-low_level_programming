5c430f60e0809390490ecb1b3ee0e23b995f24b0
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-05 23:17:09 +0200

    Task 2

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,33 +12,33 @@
  */
 int inner_binary_search(int *array, size_t start, size_t end, int value)
 {
-  size_t index = -1, middle, i;
-  int size = end - start;
+	size_t index = -1, middle, i;
+	int size = end - start;
 
-  if (array != NULL && size > 0)
-  {
-    printf("Searching in array: ");
-    for (i = start; i < end; i++)
-    {
-      printf("%d", array[i]);
-      if (i < end - 1)
-        printf(", ");
-    }
-    printf("\n");
-    if (size % 2 == 0)
-      middle = (size / 2) - 1;
-    else
-      middle = (size - 1) / 2;
-    middle += start;
-    if (array[middle] == value)
-      index = middle;
-    else if (array[middle] > value)
-      return (inner_binary_search(array, 0, middle, value));
-    else
-      return (inner_binary_search(array, middle + 1, size, value));
-  }
+	if (array != NULL && size > 0)
+	{
+		printf("Searching in array: ");
+		for (i = start; i < end; i++)
+		{
+			printf("%d", array[i]);
+			if (i < end - 1)
+				printf(", ");
+		}
+		printf("\n");
+		if (size % 2 == 0)
+			middle = (size / 2) - 1;
+		else
+			middle = (size - 1) / 2;
+		middle += start;
+		if (array[middle] == value)
+			index = middle;
+		else if (array[middle] > value)
+			return (inner_binary_search(array, 0, middle, value));
+		else
+			return (inner_binary_search(array, middle + 1, size, value));
+	}
 
-  return (index);
+	return (index);
 }
 /**
  * binary_search - search alg
@@ -53,5 +53,5 @@ int inner_binary_search(int *array, size_t start, size_t end, int value)
  */
 int binary_search(int *array, size_t size, int value)
 {
-  return (inner_binary_search(array, 0, size, value));
+	return (inner_binary_search(array, 0, size, value));
 }
